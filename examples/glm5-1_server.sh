71fc81df77ab46614598a88452fb6657dbf0d094
jianzhnie <jianzhnie@126.com>
2026-05-12 13:35:53 +0800

    Update scripts

--- examples/glm5-1_server.sh
+++ examples/glm5-1_server.sh
@@ -7,29 +7,29 @@ export HCCL_BUFFSIZE=200
 export PYTORCH_NPU_ALLOC_CONF=expandable_segments:True
 export VLLM_ASCEND_BALANCE_SCHEDULING=1
 
-model_path=models/vllm-ascend/GLM-5-w4a8 
+export MODEL_PATH="${MODEL_PATH:-/home/jianzhnie/llmtuner/hfhub/models/Eco-Tech/GLM-5.1-w8a8}"
 vllm serve \
-     $model_path \
+     $MODEL_PATH \
      --host 0.0.0.0 \
      --port 8077 \
-     --data-parallel-size 1 \
-     --tensor-parallel-size 8 \
-     --enable-expert-parallel \
      --seed 1024 \
+     --data-parallel-size 1 \
+     --tensor-parallel-size 32 \
+     --enable-expert-parallel \
      --served-model-name glm-5.1 \
      --max-num-seqs 2 \
-     --max-model-len 32768 \
+     --max-model-len 131072 \
      --max-num-batched-tokens 4096 \
      --trust-remote-code \
      --gpu-memory-utilization 0.95 \
      --quantization ascend \
+     --distributed-executor-backend ray \
      --tool-call-parser glm47 \
      --reasoning-parser glm45 \
      --enable-chunked-prefill \
      --enable-prefix-caching \
      --enable-auto-tool-choice \
      --chat-template-content-format=string \
-     --async-scheduling \
      --compilation-config '{"cudagraph_mode": "FULL_DECODE_ONLY"}' \
      --additional-config '{"fuse_muls_add": true, "multistream_overlap_shared_expert": true, "ascend_compilation_config": {"enable_npugraph_ex": true}}' \
      --speculative-config '{"num_speculative_tokens": 3, "method": "deepseek_mtp"}'
