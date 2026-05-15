4420b4fcb35c7bb61b9cc471cc33765f30dafad1
jianzhnie <jianzhnie@126.com>
2026-05-15 16:17:40 +0800

    Update examples

---
+++ examples/kimi2_pcl.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+vllm serve "/llm_workspace_1P/robin/hfhub/pcl-kimi2-stage2/kimi2-mcore2hf_step450" \
+    --distributed-executor-backend ray \
+    --tensor-parallel-size 64 \
+    --enable-expert-parallel \
+    --max-model-len 256 \
+    --max-num-batched-tokens 8192 \
+    --max-num-seqs 12 \
+    --gpu-memory-utilization 0.9 \
+    --trust-remote-code \
+    --enable-prefix-caching \
+    --enforce-eager \
+    --host 0.0.0.0 \
+    --port 8080 \
+    --hf-overrides '{"model_type":"kimi_k2_mcore","architectures":["KimiK2MCoreV1ForCausalLM"]}'
