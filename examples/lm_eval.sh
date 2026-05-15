4c6803dff92d9ff36eb548b113c9bf43a2fea0d5
jianzhnie <jianzhnie@126.com>
2026-05-15 16:17:40 +0800

    Update examples

---
+++ examples/lm_eval.sh
@@ -0,0 +1,15 @@
+# 设置缓存目录
+export HF_HOME=/llm_workspace_1P/robin/hfhub
+export HF_DATASETS_CACHE=/llm_workspace_1P/robin/hfhub/datasets
+# export HF_DATASETS_OFFLINE=1
+# export TRANSFORMERS_OFFLINE=1
+# export HF_HUB_OFFLINE=1
+
+bash tools/eval/run_lmeval.sh \
+    --model-path /llm_workspace_1P/robin/hfhub/pcl-kimi2-stage2/kimi2-mcore2hf_step450 \
+    --output-dir outputs/mcore2hf_step450 \
+    --backend api \
+    --url "0.0.0.0" \
+    --port 8080 \
+    --tasks mmlu \
+    --fewshot 5
