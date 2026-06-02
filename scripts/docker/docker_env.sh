e52d8b0514e7bdb181818767868366efd3870941
jianzhnie <jianzhnie@126.com>
2026-06-02 13:24:30 +0800

    Update docker scripts

--- scripts/docker/docker_env.sh
+++ scripts/docker/docker_env.sh
@@ -19,7 +19,6 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # ------------------------------------------
 # 1. 部署与节点配置
 # ------------------------------------------
-export NODES_FILE="${NODES_FILE:-/llm_workspace_1P/robin/EasyInfer/scripts/node_list.txt}"
 export SSH_USER_HOST_PREFIX="${SSH_USER_HOST_PREFIX:-}"
 export SSH_OPTS="${SSH_OPTS:--o BatchMode=yes -o StrictHostKeyChecking=accept-new -o ConnectTimeout=10}"
 export PARALLELISM="${PARALLELISM:-8}"
@@ -33,7 +32,8 @@ export PARALLELISM="${PARALLELISM:-8}"
 # export RUN_CONTAINER_SCRIPT="${RUN_CONTAINER_SCRIPT:-${SCRIPT_DIR}/ascend_infer_docker_run.sh}"
 # export CONTAINER_NAME="${CONTAINER_NAME:-vllm-ascend-0.18-env}"
 
+export IMAGE_DIR="${IMAGE_DIR:-/home/jianzhnie/llmtuner/hfhub/docker/image}"
 export IMAGE_NAME="ascend910c-cann8.5.1-torch2.9.0-vllm0.18.0:latest"
-export IMAGE_TAR="${IMAGE_TAR:-/llm_workspace_1P/robin/hfhub/docker/image/ascend910c-cann8.5.1-torch2.9.0-vllm0.18.0.tar}"
+export IMAGE_TAR="${IMAGE_TAR:-${IMAGE_DIR}/ascend910c-cann8.5.1-torch2.9.0-vllm0.18.0.tar}"
 export RUN_CONTAINER_SCRIPT="${RUN_CONTAINER_SCRIPT:-${SCRIPT_DIR}/ascend_infer_docker_run.sh}"
 export CONTAINER_NAME="${CONTAINER_NAME:-npuslim-env}"
